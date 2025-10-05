8c777affa77622f552cc136f946383e67e0d3a53
r4d0v1c <radovic.ra47.2022@uns.ac.rs>
2025-10-05 22:39:37 +0200

    Realizovana main skripta za pokretanje kontrolera.

--- Kontroler/main.cpp
+++ Kontroler/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include "MQTT/mqtt.h"
+#include "MQTT/SystemController.h"
+#include <thread>
+#include <chrono>
 
-#define MQTT_BROKER         "localhost"
 #define MQTT_PORT           1883
 #define MQTT_KEEPALIVE      60
 #define MQTT_CLIENT_ID      "KontrolerClient"
@@ -11,43 +12,34 @@
 #endif
 
 int main() {
-    struct mosquitto* mosq = NULL;
-    int rc;
-
-    // 1. Inicijalizacija biblioteke
-    mosquitto_lib_init();
-
-    // 2. Kreiranje novog Mosquitto klijenta
-    mosq = mosquitto_new(MQTT_CLIENT_ID, true, NULL);
-    if(!mosq) {
-        std::cerr << "Neuspesno kreiranje Mosquitto klijenta: " << mosquitto_strerror(errno) << std::endl;
-        mosquitto_lib_cleanup();
-        return -1;
+    SystemController controller;
+    if(!controller.connect("localhost", MQTT_PORT, MQTT_KEEPALIVE)) {
+        std::cerr << "Neuspešno povezivanje na MQTT broker." << std::endl;
+        return 1;
     }
 
-    // 3. Podesavanje callback funkcija
-    mosquitto_connect_callback_set(mosq, on_connect);
-    mosquitto_message_callback_set(mosq, on_message);
-
-    // 4. Povezivanje na MQTT broker
-    rc = mosquitto_connect(mosq, MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE);
-    if(rc) {
-        std::cerr << "Neuspesno povezivanje na broker: " << mosquitto_strerror(rc) << std::endl;
-        mosquitto_destroy(mosq);
-        mosquitto_lib_cleanup();
-        return -1;
-    }
-
-    // 5. Pokretanje petlje za obradu mrežnih događaja
-    mosquitto_loop_start(mosq);
-    std::cout << "Pritisni Enter za izlaz..." << std::endl;
-    std::cin.get();
-    mosquitto_loop_stop(mosq, true);
-
-    // 6. Čišćenje resursa
-    mosquitto_disconnect(mosq);
-    mosquitto_destroy(mosq);
-    mosquitto_lib_cleanup();
+    std::cout << "Sistem kontroler pokrenut. Pritisnite Ctrl+C za zaustavljanje." << std::endl;
     
+    // Sačekaj malo da stigne inicijalna poruka sa senzora
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    // Glavna kontrolna petlja
+    while(controller.isConnected()) {
+        // Pozovi kontrolni sistem koji analizira stanje i upravlja aktuatorima
+        controller.controlSystem();
+        
+        // Prikaži status sistema
+        controller.printStatus();
+        
+        // Prikaži alarme
+       // controller.printAlarms();
+        
+        // Pauza između iteracija (npr. 2 sekundi)
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
+    std::cout << "Kontroler se isključuje..." << std::endl;
+    controller.disconnect();
+
     return 0;
 }
